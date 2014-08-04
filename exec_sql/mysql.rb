65c36d7c90b4611c273b46e90610b1e0a3c8e73e
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2014-08-04 21:34:38 +0900

    SQL実行ツール * MySQL対応

---
+++ exec_sql/mysql.rb
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+#
+#  Copyright 2014 agwlvssainokuni
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+require 'open3'
+require File.join(File.dirname(__FILE__), 'logger')
+
+module ExecSql
+
+  # MySQL対応
+  class MySQL
+
+    # インスタンス初期化
+    def initialize(conf, basedir)
+      @conf = (conf.nil? || conf.empty?) ? "#{basedir}/execsql-my.cnf" : conf
+    end
+
+    # SQL実行
+    def execute(files, force)
+      @logger = Logger.new("EXECSQL[MySQL]")
+      @logger.debug("start")
+
+      argv = ["--default-extra-file", @conf]
+      argv << "--force" if force
+
+      files = files.lazy unless force
+      status = files.map {|f|
+        @logger.debug("Executing %s", f)
+        Open3.popen2e("mysql", *argv) {|si, so, th|
+
+          IO.foreach(f) {|l| si << l}
+          si.close_write
+
+          st = th.value
+          if st.success?
+            @logger.notice("mysql < %s: OK, status=%d", st)
+          else
+            @logger.error("mysql < %s: NG, status=%d, out=%s", st,
+                          so.readlines.join
+          end
+
+          st
+        }
+      }.find {|st|
+        st.success?
+      }
+
+      if status.nil? || status.success?
+        @logger.debug("end normally")
+        return 0
+      else
+        @logger.debug("end abnormally")
+        return status
+      end
+    end
+
+  end
+end
